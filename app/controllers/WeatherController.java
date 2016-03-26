e7f1826ba56cc793a0dc3ffb7c0221e35d19fa7b
kenshinji <gundam0083ster@gmail.com>
2016-03-26 23:49:20 +0800

    finished an endpoint quering weather by Yahoo weather API

---
+++ app/controllers/WeatherController.java
@@ -0,0 +1,23 @@
+package controllers;
+
+
+import play.libs.ws.WSClient;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+import javax.inject.Inject;
+import java.util.concurrent.CompletionStage;
+
+/**
+ * Created by kenshinji on 3/26/16.
+ */
+public class WeatherController extends Controller {
+    @Inject WSClient ws;
+    private static final String URL = "https://query.yahooapis.com/v1/public/yql";
+    public CompletionStage<Result> get(String city) {
+        String q1 = "select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text=\"" + city +"\")";
+        return ws.url(URL).setQueryParameter("q", q1).setQueryParameter("format", "json").get().thenApply(response ->
+                ok(response.asJson())
+        );
+    }
+}
